3b04f8d10c132cf0ab1f848cf1c71b58663a4a4c
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2023-09-04 14:08:13 +0600

    templates added and settings.py configured

--- base/views.py
+++ base/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home Page')
+    return render(request, 'home.html')
 
 def room(request):
-    return HttpResponse('Chat Rooms')
+    return render(request, 'room.html')
