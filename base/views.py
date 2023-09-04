8caaaec404861662ac3009337fa58ee06dc33cd0
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2023-09-04 16:39:16 +0600

    added database for room in views.py

--- base/views.py
+++ base/views.py
@@ -1,21 +1,19 @@
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
 
-rooms = [
-    {'id':1, 'name': 'Lets Learn Python!'},
-    {'id':2, 'name': 'Frontend developers'},
-    {'id':3, 'name': 'Backend developers'},
-]
+# rooms = [
+#     {'id':1, 'name': 'Lets Learn Python!'},
+#     {'id':2, 'name': 'Frontend developers'},
+#     {'id':3, 'name': 'Backend developers'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
