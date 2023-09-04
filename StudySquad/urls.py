07132bd900e4a56b61a03c47941b118977dfdce3
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2023-09-04 13:31:28 +0600

    Update urls.py

--- StudySquad/urls.py
+++ StudySquad/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Home Page')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 ]
