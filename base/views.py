5988d287c85190c1bd7f7e0faa38d770b68bc839
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2023-09-06 20:53:26 +0600

    profile setup (bug fixed)

--- base/views.py
+++ base/views.py
@@ -95,9 +95,10 @@ def room(request, pk):
     context = {'room': room, 'room_messages': room_messages, 'participants':participants}
     return render(request, 'base/room.html', context)
 
-def userProfile(request):
-    context = {}
-    return render(request, 'base.profile.html', context)
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    context = {'user': user}
+    return render(request, 'base/profile.html', context)
     
 
 @login_required(login_url='login')
