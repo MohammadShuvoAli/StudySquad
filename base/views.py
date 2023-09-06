41c24a2e39f9bbccb44aa5195543356ef0e42a5f
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2023-09-06 14:47:29 +0600

    login required

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -57,6 +58,8 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+@login_required(login_url='login')
 
 def createRoom(request):
     form = RoomForm()
