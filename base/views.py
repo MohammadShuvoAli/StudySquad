6514c1665afb4885710dc41eb9f27f9dc3f66efa
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2023-09-06 13:16:24 +0600

    Search Updated

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -13,7 +14,11 @@ from .forms import RoomForm
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) | 
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
     topics = Topic.objects.all()
     
     context = {'rooms':rooms, 'topics':topics}
