c7e76eb7d101ed193585c0136682484b6f065ff4
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2023-09-22 17:33:48 +0600

    room complete

--- base/views.py
+++ base/views.py
@@ -126,10 +126,13 @@ def updateRoom(request, pk):
         return HttpResponse('You are not allowed here!!')
     
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name = topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
     
     context = {'form': form, 'topics': topics, 'room': room}
     return render (request, 'base/room_form.html', context)
