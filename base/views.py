a63c81f3accf21614515cc929f94a866bfd7fd41
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2023-09-25 23:51:19 +0600

    activity tab for smartphone

--- base/views.py
+++ base/views.py
@@ -176,3 +176,7 @@ def topicsPage(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     topics = Topic.objects.filter(name__icontains=q)
     return render(request, 'base/topics.html', {'topics':topics})
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {})
