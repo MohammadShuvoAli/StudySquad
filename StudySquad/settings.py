fed855426dc37fd6922d05e67af0f476b7559b31
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2023-09-27 20:11:06 +0600

    django cors headers added for api

--- StudySquad/settings.py
+++ StudySquad/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
     
     'rest_framework',
+    
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -51,6 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'StudySquad.urls'
@@ -131,3 +136,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS = True
