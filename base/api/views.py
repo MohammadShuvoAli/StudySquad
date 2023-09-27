b8c84fdbb5c593111d2bddc3dad8fc383d66a746
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2023-09-27 19:44:01 +0600

    rest api added

---
+++ base/api/views.py
@@ -0,0 +1,11 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        'GET /api',
+        'GET /api/rooms',
+        'GET /api/rooms/:id'
+    ]
+    return Response(routes)
