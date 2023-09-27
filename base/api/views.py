f18103e38020bdd40b6db654e108983f4ee224a2
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2023-09-27 19:53:42 +0600

    Serializer added

--- base/api/views.py
+++ base/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from base.models import Room
+from .serializers import RoomSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -9,3 +11,8 @@ def getRoutes(request):
         'GET /api/rooms/:id'
     ]
     return Response(routes)
+
+@api_view(['GET'])
+def getRooms(request):
+    rooms = Room.objects.all()
+    return Response(rooms)
