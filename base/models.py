c68986b7ce3bf214b05d150153dc50b9a67ac64c
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2023-09-28 19:42:56 +0600

    initial setting for avatar

--- base/models.py
+++ base/models.py
@@ -6,7 +6,8 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
     
-    #avatar = 
+    avatar = models.ImageField(null=True, default="avatar.svg")
+    
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
